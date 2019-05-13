3d67e1ac81b06c17a8c633fcceebb6fe85c2d9a5
Jeff Lapp <lappjeff20@gmail.com>
2019-05-13 16:00:46 -0400

    Removed all unnecessary folders

--- src/main/java/com/lambdaschool/orders/repos/OrdersRepository.java
+++
@@ -1,8 +0,0 @@
-package com.lambdaschool.orders.repos;
-
-import com.lambdaschool.orders.model.Orders;
-import org.springframework.data.repository.CrudRepository;
-
-public interface OrdersRepository extends CrudRepository<Orders, Long>
-{
-}
